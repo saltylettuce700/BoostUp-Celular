5da60d4f09dcb383ab67a549f6debb5077caa132
saltylettuce700 <dana.moravi@gmail.com>
2025-02-09 21:29:24 -0600

    ver detalles xml ver detalles de producto xml

---
+++ app/src/main/java/com/example/user/Activity/Registro/ver_detalles_activity.java
@@ -0,0 +1,28 @@
+package com.example.user.Activity.Registro;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.user.R;
+
+public class ver_detalles_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_ver_detalles);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //cuando sea un producto de saborizante, la seccion de precauciones desaparece
+    }
+}
