e88838f7eebba5e6bc9be2e9d19368f2bb3b29b3
saltylettuce700 <dana.moravi@gmail.com>
2025-05-04 19:27:18 -0600

    ver detalles activity con imagenes y traduccion

--- app/src/main/java/com/example/user/Activity/Registro/ver_detalles_activity.java
+++ app/src/main/java/com/example/user/Activity/Registro/ver_detalles_activity.java
@@ -1,7 +1,13 @@
 package com.example.user.Activity.Registro;
 
+import static com.example.user.Activity.account_activity.LANGUAGE_PREF;
+import static com.example.user.Activity.account_activity.SELECTED_LANGUAGE;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,20 +18,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.user.Activity.account_activity;
+import com.example.user.Activity.catalogo_producto_activity;
 import com.example.user.Activity.historial_activity;
 import com.example.user.ConexionBD.BD;
 import com.example.user.R;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.mlkit.nl.translate.TranslateLanguage;
+import com.google.mlkit.nl.translate.Translation;
+import com.google.mlkit.nl.translate.Translator;
+import com.google.mlkit.nl.translate.TranslatorOptions;
 
 public class ver_detalles_activity extends AppCompatActivity {
 
     TextView nombretxt, fechatxt, marcatxt, tipotxt, cont_nutritxt, precauciontxt, textoPrecauciones, textoTipo, textoDescNutri;
 
+    TextView tvNombreProteina, tvTipo;
+    private Translator translator;
+
+    ImageView imgProducto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Cargar el idioma guardado y aplicarlo
+        String languageCode = loadLanguagePreference();
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ver_detalles);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -46,6 +66,17 @@ public class ver_detalles_activity extends AppCompatActivity {
         textoPrecauciones = findViewById(R.id.tvPrecauciones);
         textoTipo = findViewById(R.id.tvTipo);
         textoDescNutri = findViewById(R.id.tvDescripcionNutricional);
+
+        imgProducto = findViewById(R.id.imageBackground);
+
+        tvNombreProteina = findViewById(R.id.tvNombreProteina);
+        tvTipo = findViewById(R.id.tvTipo);
+
+
+
+
+        // Configura el traductor
+        setupTranslator();
 
 
         BD bd = new BD(ver_detalles_activity.this);
@@ -79,6 +110,14 @@ public class ver_detalles_activity extends AppCompatActivity {
                                     cont_nutritxt.setText(cont_nutri);
                                     textoPrecauciones.setText("Alergenos:");
                                     precauciontxt.setText(texto);
+
+                                    if (loadLanguagePreference().equals("en")) {
+                                        translateSelectedTextViews();
+                                    }
+
+                                    int resID = obtenerImagenPorProducto("Proteínas", marca);
+
+                                    imgProducto.setImageResource(resID);
 
 
                                 });
@@ -122,6 +161,16 @@ public class ver_detalles_activity extends AppCompatActivity {
                         textoPrecauciones.setText(texto);
                         precauciontxt.setText(sabor);
 
+                        if (loadLanguagePreference().equals("en")) {
+                            translateSelectedTextViews();
+                        }
+
+                        int resID = obtenerImagenPorProducto("Saborizantes", sabor);
+
+                        imgProducto.setImageResource(resID);
+
+
+
                     });
                 }
 
@@ -151,6 +200,14 @@ public class ver_detalles_activity extends AppCompatActivity {
                         textoDescNutri.setText("Contenido gingerol (gr)");
                         cont_nutritxt.setText(""+ cont_gingerol);
                         precauciontxt.setText(precauciones);
+
+                        if (loadLanguagePreference().equals("en")) {
+                            translateSelectedTextViews();
+                        }
+
+
+                        int resID  = obtenerImagenPorProducto("Cúrcuma y Jengibre", marca);
+                        imgProducto.setImageResource(resID);
                     });
                 }
 
@@ -167,9 +224,83 @@ public class ver_detalles_activity extends AppCompatActivity {
 
         findViewById(R.id.imageButton).setOnClickListener(v -> {
             finish();
-            startActivity(new Intent(this, historial_activity.class));
+            startActivity(new Intent(this, catalogo_producto_activity.class));
             Toast.makeText(this, "PROGRESO", Toast.LENGTH_SHORT).show();
 
         });
     }
+
+    // Cargar el idioma guardado
+    private String loadLanguagePreference() {
+        SharedPreferences preferences = getSharedPreferences(LANGUAGE_PREF, MODE_PRIVATE);
+
+        Toast.makeText(this, "Idioma actual: " + preferences.getString(SELECTED_LANGUAGE, "es"), Toast.LENGTH_SHORT).show(); // Verifica el idioma
+
+        return preferences.getString(SELECTED_LANGUAGE, "es"); // Default is Spanish
+
+    }
+
+    // Configurar el traductor ML Kit
+    private void setupTranslator() {
+        TranslatorOptions options = new TranslatorOptions.Builder()
+                .setSourceLanguage(TranslateLanguage.SPANISH)
+                .setTargetLanguage(TranslateLanguage.ENGLISH)
+                .build();
+
+        translator = Translation.getClient(options);
+
+        translator.downloadModelIfNeeded()
+                .addOnSuccessListener(aVoid -> Toast.makeText(this, "Modelo descargado", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(this, "Error al descargar el modelo: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+    }
+
+
+    private void translateSelectedTextViews() {
+        translateTextView(fechatxt);
+        translateTextView(tipotxt);
+        translateTextView(cont_nutritxt);
+        translateTextView(textoPrecauciones);
+        translateTextView(precauciontxt);
+        translateTextView(textoDescNutri);
+
+        translateTextView(tvNombreProteina);
+        translateTextView(tvTipo);
+
+    }
+
+    private void translateTextView(TextView textView) {
+        String originalText = textView.getText().toString();
+        translator.translate(originalText)
+                .addOnSuccessListener(translatedText -> textView.setText(translatedText))
+                .addOnFailureListener(e -> Toast.makeText(this, "Error al traducir: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+    }
+
+    private int obtenerImagenPorProducto(String categoria, String valorClave) {
+        if (valorClave == null) return 0;
+
+        valorClave = valorClave.toLowerCase();
+
+        switch (categoria) {
+            case "Proteínas":
+                if (valorClave.contains("hero sport")) return R.drawable.pure_natural_img;
+                if (valorClave.contains("birdman")) return R.drawable.falcon;
+                return 0;
+
+            case "Saborizantes":
+                if (valorClave.contains("fresa")||valorClave.contains("strawberry")) return R.drawable.strawberry_pink;
+                if (valorClave.contains("chocolate")) return R.drawable.choco_cafe;
+                if (valorClave.contains("vainilla")|| valorClave.contains("vanilla")) return R.drawable.vanilla_yellow;
+
+
+                return 0;
+
+            case "Cúrcuma y Jengibre":
+                if (valorClave.contains("nature heart")) return R.drawable.nature_heart_turmeric;
+                return 0;
+        }
+
+        return 0;
+    }
+
+
 }
